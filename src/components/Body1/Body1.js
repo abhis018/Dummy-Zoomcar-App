a77016cd5cd575b08c90f14051ee41441d88c56b
abhis018 <76279317+abhis018@users.noreply.github.com>
2021-03-06 02:35:16 +0530

    editing in style of the layout
    Signed-off-by: abhis018 <76279317+abhis018@users.noreply.github.com>

--- src/components/Body1/Body1.js
+++ src/components/Body1/Body1.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import './Body.css'
+import './Body.css';
+import img1 from './image1.png';
+import img2 from './image2.png';
+import img3 from './image3.png';
+import img4 from './image4.png';
+import img5 from './image5.png';
+import img6 from './image6.png';
 
 const Body1 = () => {
     return(
@@ -14,28 +20,34 @@ const Body1 = () => {
                 <p>We simplified car rentals, so you can focus on what's important to you.</p>
                 <div className="container">
                     <div className="container1">
+                        <img src={img1}/>
                         <h3>Fuel Cost Included</h3>
                         <p className="para">Don't worry about mileage! All fuel costs are Included. If you refill fuel, we'll pay you back!</p>
                     </div>
                     <div className="container1">
+                        <img src={img2}/>
                         <h3>No Hidden Charges</h3>
                         <p className="para">Our prices include taxes and insurance.<br/> What you see is what you really pay!</p>
                     </div>
                     <div className="container1">
+                    <img src={img3}/>
                         <h3>Flexi Pricing Packages</h3>
                         <p className="para">One size never fits all! Choose a balance of<br/> time and kilometers that works best for you.</p>
                     </div>
                 </div>
                 <div className="container">
                     <div className="container1">
+                        <img src={img4}/>
                         <h3>Go Anywhere</h3>
                         <p className="para">Our cars have all-India permits. Just<br/> remember to pay state tolls and entry taxes.</p>
                     </div>
                     <div className="container1">
+                        <img src={img6}/>
                         <h3>23x7 Roadside Assistance</h3>
                         <p className="para">We have round-the-clock, pan India<br/> partners. Help is never far away from you.</p>
                     </div>
                     <div className="container1">
+                        <img src={img5}/>
                         <h3>Damage Insurance</h3>
                         <p className="para">All your bookings include damage insurance! Drive safe, but don’t worry!</p>
                     </div>
--- src/components/Body2/Body2.js
+++ src/components/Body2/Body2.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import './Body2.css';
 import pic from './pic1.svg';
+import dl from './dl.svg';
+import Return from './return.svg';
+import book from './book.svg';
+import unlock from './unlock.png';
+import zoom from './zoom.svg'; 
+
 
 const Body2 = () =>{
     return(
@@ -18,27 +24,35 @@ const Body2 = () =>{
                 <p className="info">Drive yourself to an adventure and back in 5 simple steps</p>
                 <div className="grid">
                     <div className="container-1">
+                        <img className="img" src={book}/>
                         <h4>BOOK</h4>
                         <p>Search for and book a car on<br/> our site!</p>
                     </div>
                     <div className="container-1">
+                        <img className="img" src={dl}/>
                         <h4>UPLOAD LICENSE</h4>
                         <p>Upload your driver's license,<br/> and pay a small security<br/>  deposit.</p>
                     </div>
                     <div className="container-1">
+                        <img className="unlock" src={unlock}/>
                         <h4>UNLOCK</h4>
                         <p>We SMS your car details 20<br/>  minutes before pickup. Unlock<br/>  it via the Zoomcar app.</p>
                     </div>
                     <div className="container-1">
+                        <img className="img" src={zoom}/>
                         <h4>ZOOM</h4>
                         <p>Fill the start checklist in the<br/>  Zoomcar app. Grab the keys<br/>  from the glove-box and drive.</p>
                     </div>
                     <div className="container-1">
+                        <img className="img" src={Return}/>
                         <h4>RETURN</h4>
                         <p>Return the car to the same<br/>  location and fill the end<br/>  checklist to end your trip.</p>
                     </div>
                 </div>
                 <button className="but">SIGN UP</button>
+            </div>
+            <div className="points">
+                <p>EARN Z POINTS</p>
             </div>
         </div>
     )
